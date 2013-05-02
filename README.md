2527e987432c08f3d1cc3e6fa33a72ab8dfb0942
Joshua Galvin <jgalvin@fireforgeeast.com>
2013-05-02 04:15:03 -0400

    Initial commit of moctx.js

---
+++ README.md
@@ -0,0 +1,43 @@
+MOCTX.js 
+--------
+
+MOCTX.js is a monad wrapper around the CanvasRenderingContext2D Object built on top of Douglas Crockford's awesome Monad library. 
+
+The entire standards compliant interface is available and chainable. Any properties are available as setters and getters.
+
+
+Examples
+--------
+
+Drawing a red square:
+
+  MOCTX().setFillStyle("rgb(200,0,0)").fillRect(0, 0, 20, 20);
+
+Passing an existing canvas 2D context and drawing a line:
+
+  var canvas = document.getElementById("someCanvas");
+  var ctx = canvas.getContext("2d");
+
+  MOCTX(ctx)
+    .setStrokeStyle("rgb(0, 0, 0)")
+    .beginPath()
+    .moveTo(0, 50)
+    .lineTo(100, 50)
+    .fill();
+
+Lets get the canvas reference after drawing a circle:
+
+  var canvas = MOCTX()
+    .beginPath()
+    .arc(75, 75, 50, 0, Math.PI*2, true)
+    .getCanvas();
+
+
+Oh and I added helper functions to set the width and the height of the canvas:
+
+  MOCTX()
+    .setWidth(200)
+    .setHeight(400);
+
+  MOCTX()
+    .setSize(200, 400);
---
+++ moctx.js
@@ -0,0 +1,98 @@
+// MOCTX.js
+// Joshua Galvin
+// 2013-05-23
+
+// MIT License
+
+var MOCTX = (function () {
+  'use strict';
+
+  function XMONAD(modifier) {
+    var monad = MONAD(modifier);
+
+    monad.properties = function () {
+      Array.prototype.slice.call(arguments, 0).forEach(function (name) {
+        var upName = name.charAt(0).toUpperCase() + name.slice(1);
+
+        monad.lift("set" + upName, function (value, mod) {
+          value[name] = mod;
+          return value;
+        });
+
+        monad.lift_value("get" + upName, function (value) {
+          return value[name];
+        });
+      });
+      return this;
+    };
+
+    monad.lifts = function () {
+      Array.prototype.slice.call(arguments, 0).forEach(function (name) {
+        monad.lift(name, function (value) {
+          if (typeof value[name] === "function") {
+            value[name].apply(value, Array.prototype.slice.call(arguments, 1));
+          }
+          return value;
+        });
+      });
+      return this;
+    };
+
+    monad.lift_values = function () {
+      Array.prototype.slice.call(arguments, 0).forEach(function (name) {
+        monad.lift_value(name, function (value) {
+          if (typeof value[name] === "function") {
+            return value[name].apply(value, Array.prototype.slice.call(arguments, 1));
+          }
+          return null;
+        });
+      });
+      return this;
+    };
+
+    return monad;
+  }
+
+  var MOCTX = XMONAD(function (monad, value) {
+    if (value === undefined) {
+      var canvas = document.createElement("canvas");
+      document.body.appendChild(canvas);
+      return canvas.getContext("2d");
+    }
+    return value;
+  }).lifts("arc", "arcTo", "beginPath", "bezierCurveTo", "clearRect")
+    .lifts("clip", "closePath", "drawImage", "drawSystemFocusRing", "fill", "fillRect")
+    .lifts("fillText", "lineTo", "moveTo", "putImageData", "quadraticCurveTo", "rect", "restore")
+    .lifts("rotate", "save", "scale", "scrollPathIntoView", "setLineDash", "setTransform", "stroke")
+    .lifts("strokeRect", "strokeText", "transform", "translate")
+    .lift_values("createImageData", "createLinearGradient", "createPattern", "createRadialGradient")
+    .lift_values("drawCustomFocusring", "getImageData", "getLineDash", "isPointInPath", "isPointInStroke")
+    .lift_values("measureText")
+    .properties("canvas", "fillStyle", "font", "globalAlpha", "globalCompositeOperation", "lineCap")
+    .properties("lineDashOffset", "lineJoin", "lineWidth", "miterLimit", "shadowBlur", "shadowColor")
+    .properties("shadowOffsetX", "shadowOffsetY", "strokeStyle", "textAlign", "textBaseLine")
+    .lift("setWidth", function (ctx, width) {
+      ctx.canvas.setAttribute("width", width);
+      return ctx;
+    })
+    .lift("setHeight", function (ctx, height) {
+      ctx.canvas.setAttribute("height", height);
+      return ctx;
+    })
+    .lift("setSize", function (ctx, width, height) {
+      ctx.canvas.setAttribute("height", height);
+      ctx.canvas.setAttribute("width", width);
+      return ctx;
+    })
+    .lift_value("getHeight", function (ctx) {
+      return ctx.canvas.getAttribute("height");
+    })
+    .lift_value("getWidth", function (ctx) {
+      return ctx.canvas.getAttribute("width");
+    })
+    .lift_value("getSize", function (ctx) {
+      return [ctx.canvas.getAttribute("height"), ctx.canvas.getAttribute("width")];
+    });
+
+  return MOCTX;
+}());
